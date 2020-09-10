824d50b5a3d7c2b12e295ec8154e12938dcf7f27
CJ Davis <csdj92@gmail.com>
2020-09-10 14:14:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
 elsif array.size == 2
     array.join(" and ")
   else array.size == 3 
-    array.join(" , ") << (", and ")
+    array.join(" , ") << (", and #{array.last}")
     
   end
 end
