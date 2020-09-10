3e7a6b0396ad5c980eae3d91156b02cc195ecee8
CJ Davis <csdj92@gmail.com>
2020-09-10 13:47:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.length <3 || 2>
-    array.join(and)
+    array.join("and")
   else
     large-array=array
     large-array-=1
-    large-array.join (,)
+    large-array.join (",")
   end
 end
 
