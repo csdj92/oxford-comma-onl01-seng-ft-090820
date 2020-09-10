feb0da4628a044dc03605f5933a25d12b91237ad
CJ Davis <csdj92@gmail.com>
2020-09-10 14:08:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size < 1
     array
 elsif array.size < 2
-    array.join(" and #{array[-1]} ")
+    array.join(" and ")
   else 
     array[0...-1].join(", ")
   end
