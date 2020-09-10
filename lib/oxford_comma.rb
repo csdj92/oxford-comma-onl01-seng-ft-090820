07b5836ca3948a73375a8e44d13d62979aa4ab99
CJ Davis <csdj92@gmail.com>
2020-09-10 14:14:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
 elsif array.size == 2
     array.join(" and ")
   else array.size == 3 
-    array.push (" , " " and ")
+    array.join(" , ") << (", and ")
     
   end
 end
